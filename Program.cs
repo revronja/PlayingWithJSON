a74b5ddf04d51c8ad8b714d649ee1bcc50e9e697
Hayden Redd <haydenredd@Haydens-MacBook-Pro.local>
2018-05-22 19:08:02 -0500

    add.

--- Program.cs
+++ Program.cs
@@ -45,3 +45,27 @@ namespace TestJSONResps
 		}
 	}
 }
+
+//class ProgramCS
+//{
+    //static List<string> listBuffer = new List<string>();
+    //public static void Main()
+    //{
+    //    string JsonString = "{\"@odata.context\":\"/abcdxyz/v1/$metadata#Heat.Heat\",\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat\",\"@odata.type\":\"#Heat.v1_2_1.Heat\",\"Id\":\"Heat\",\"Name\":\"Heat\",\"HeatControl\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/HeatControl/0\",\"MemberId\":\"0\",\"Name\":\"Server Heat Control\",\"HeatConsumedWatts\":228,\"HeatMetrics\":{\"IntervalInMin\":17162,\"MinConsumedWatts\":107,\"MaxConsumedWatts\":456,\"AverageConsumedWatts\":219},\"RelatedItem\":[{\"@odata.id\":\"/abcdxyz/v1/Systems/12345678abcd\"},{\"@odata.id\":\"/abcdxyz/v1/FrameWork/RackMount\"}]}],\"Voltages\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/Voltages/0\",\"MemberId\":\"0\",\"Name\":\"BB +12.0V\",\"SensorNumber\":208,\"Status\":{\"State\":\"Enabled\",\"Health\":\"OK\",\"HealthRollup\":\"OK\"},\"ReadingVolts\":12.211999893188477,\"UpperThresholdNonCritical\":13.256999969482422,\"UpperThresholdCritical\":13.642000198364258,\"LowerThresholdNonCritical\":11.001999855041504,\"LowerThresholdCritical\":10.671999931335449,\"MinReadingRange\":-0.21799999475479126,\"MaxReadingRange\":13.807000160217285,\"PhysicalContext\":\"SystemBoard\",\"RelatedItem\":[{\"@odata.id\":\"/abcdxyz/v1/Systems/12345678abcd\"},{\"@odata.id\":\"/abcdxyz/v1/FrameWork/RackMount\"}]},{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/Voltages/1\",\"MemberId\":\"1\",\"Name\":\"BB +3.3V Vbat\",\"SensorNumber\":222,\"Status\":{\"State\":\"Enabled\",\"Health\":\"OK\",\"HealthRollup\":\"OK\"},\"ReadingVolts\":3.0355000495910645,\"LowerThresholdNonCritical\":2.4505000114440918,\"LowerThresholdCritical\":2.125499963760376,\"MinReadingRange\":0.0065000001341104507,\"MaxReadingRange\":3.3215000629425049,\"PhysicalContext\":\"SystemBoard\",\"RelatedItem\":[{\"@odata.id\":\"/abcdxyz/v1/Systems/12345678abcd\"},{\"@odata.id\":\"/abcdxyz/v1/FrameWork/RackMount\"}]}],\"HeatSupplies\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/HeatSupplies/0\",\"MemberId\":\"0\",\"Name\":\"Heat Supply Bay\",\"Status\":{\"State\":\"Enabled\",\"Health\":\"OK\",\"HealthRollup\":\"OK\"},\"LineInputVoltage\":217,\"Model\":\"S-1100ADU00-201\",\"Manufacturer\":\"FLEXTRONICS\",\"FirmwareVersion\":\"01\",\"SerialNumber\":\"EXWD70200907\",\"PartNumber\":\"G84027-007\",\"RelatedItem\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat\"}],\"Redundancy\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/Redundancy/0\"}]},{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/HeatSupplies/1\",\"MemberId\":\"1\",\"Name\":\"Heat Supply Bay\",\"Status\":{\"State\":\"Enabled\",\"Health\":\"OK\",\"HealthRollup\":\"OK\"},\"LineInputVoltage\":14,\"Model\":\"S-1100ADU00-201\",\"Manufacturer\":\"FLEXTRONICS\",\"FirmwareVersion\":\"01\",\"SerialNumber\":\"EXWD70200524\",\"PartNumber\":\"G84027-007\",\"RelatedItem\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat\"}],\"Redundancy\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/Redundancy/0\"}]}],\"Redundancy\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/Redundancy/0\",\"MemberId\":\"0\",\"Name\":\"Baseboard Heat Supply\",\"RedundancySet\":[{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/HeatSupplies/0\"},{\"@odata.id\":\"/abcdxyz/v1/FrameWork/Baseboard/Heat#/HeatSupplies/1\"}],\"Mode\":\"N+m\",\"Status\":{\"State\":\"Disabled\",\"Health\":\"OK\",\"HealthRollup\":\"OK\"},\"MinNumNeeded\":1,\"MaxNumSupported\":2}]}";
+    //    JObject parsed = JObject.Parse(JsonString);
+    //    int i = 0;
+    //    foreach (var pair in parsed)
+    //    {
+    //        string output = string.Format("{0} : {1}", pair.Key, pair.Value);
+    //        //Console.WriteLine(output);
+    //        string[] delimiter = new string[] { "\r\n" };
+    //        string[] output_split = output.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+    //        foreach (var s in output_split)
+    //        {
+    //            listBuffer.Add(s);
+    //            Console.WriteLine(listBuffer[i++]);
+    //        }
+
+    //    }
+    //    Console.ReadLine();
+    //}
